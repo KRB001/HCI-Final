6c1f9c489cca41a116e0052ff6a9706d59cfc587
KRB001 <kieranbentley0@gmail.com>
2023-12-05 11:50:04 -0500

    Skill point validator

--- app/forms.py
+++ app/forms.py
@@ -6,14 +6,19 @@ from app.models import User, Player, PlayerClass, PlayerRace
 
 class CreateCharacterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    str = StringField('Strength', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
-    dex = StringField('Dexterity', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
-    con = StringField('Constitution', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
-    int = StringField('Intelligence', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
-    wis = StringField('Wisdom', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
-    cha = StringField('Charisma', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
+    strength = StringField('Strength', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
+    dexterity = StringField('Dexterity', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
+    constitution = StringField('Constitution', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
+    intelligence = StringField('Intelligence', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
+    wisdom = StringField('Wisdom', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
+    charisma = StringField('Charisma', validators=[DataRequired(), NumberRange(3, 20, "Stat must be in the range 3 - 20")])
     level = StringField('Level', validators=[DataRequired(), NumberRange(1, 20, "Level must be in the range 1 - 20")])
     xp = StringField('XP')
     player_class = SelectField('Class', validators=[DataRequired()], coerce=int)
     player_race = SelectField('Race', validators=[DataRequired()], coerce=int)
     submit = SubmitField('Update Character')
+
+    def validate_stats(self, level, strength, dexterity, constitution, intelligence, wisdom, charisma):
+        max_skill_points = 16 + int(level)
+        if int(strength) + int(dexterity) + int(constitution) + int(intelligence) + int(wisdom) + int(charisma) > max_skill_points:
+            raise ValidationError("Number of skill points expended exceeds " + str(max_skill_points))
